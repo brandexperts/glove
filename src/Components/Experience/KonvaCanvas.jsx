6d559e675b94bd4c8d05a28bcc4e51843df8f46e
ahmedrana3d <af164021@gmail.com>
2024-12-09 18:15:11 +0500

    aligning text in center

--- src/Components/Experience/KonvaCanvas.jsx
+++ src/Components/Experience/KonvaCanvas.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Stage as KonvaStage, Layer, Rect, Text } from 'react-konva'
+import { Stage as KonvaStage, Layer, Text } from 'react-konva'
 import { useTextConfig } from '../UI/TextInputUI'
 
 const KonvaCanvas = () => {
@@ -8,29 +8,23 @@ const KonvaCanvas = () => {
   return (
     <>
       <KonvaStage
-        className="text-center absolute z-40  hidden  bg-transparent"
+        className="text-center absolute z-40 hidden bg-transparent"
         id="konva"
         width={400}
         height={400}
       >
-
-
-{/* 220 349 */}
-
         <Layer>
-
-        {/* <Rect draggable fill={"red"} width={400} height={400}/> */}
-
           <Text
             text={textInput}
             fontFamily="OE1Bold"
             fill={"#FFF"}
             fontSize={38 + scale}
             draggable
-            x={220 + (scale *0.5)} // Center horizontally (400 / 2)
-            y={349} // Center vertically (400 / 2)
-            offsetX={(textInput.length * (30 + scale)) / 2} // Adjust based on text length and font size
-            offsetY={((38 + scale) / 2)} // Adjust for the height of the text (fontSize + scale)
+            x={12} 
+            y={324} 
+            align='center'
+            width={400}
+            height={400}
             onDragMove={(e) => {
               console.log(e.target.x(), e.target.y());
             }}
