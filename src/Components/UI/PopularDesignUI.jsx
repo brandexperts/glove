46f9a7cc7bf9292e80e7a61e4ca6885fcd2d55a4
ahmedrana3d <af164021@gmail.com>
2024-12-16 21:49:41 +0500

    Assingning Var to Back Logos

--- src/Components/UI/PopularDesignUI.jsx
+++ src/Components/UI/PopularDesignUI.jsx
@@ -7,6 +7,14 @@ const useDesignStore = create((set) => ({
   }));
 
   const PopularDesignUI = () => {
+
+
+const NegroClasico = "./color-options/boxing-logos/boxing_logo.png"
+const BlancoClasico = "./color-options/boxing-logos/boxing_logo_white.png"
+const VivaMexico = "./color-options/boxing-logos/boxing_logo_color.png"
+
+  
+
     // Sample design options
     const designOptions = [
       {
@@ -20,21 +28,38 @@ const useDesignStore = create((set) => ({
         insideThumbCover: { metalic: true, color: "#0072CE" },
         trim: { metalic: true, color: "#0072CE" },
         laces: { metalic: true, color: "#0072CE" },
-        backLogo : "./color-options/boxing-logos/boxing_logo_color.png"
+        backLogo : VivaMexico
       },
       {
-        Name: "Second",
-        img : "/color-options/popular-designs/red.png",
-        fist: { metalic: false, color: "red" },
-        frontThumb: { metalic: false, color: "red" },
-        wristColor: { metalic: false, color: "red" },
-        internalPalm: { metalic: false, color: "red" },
-        innerThumb: { metalic: false, color: "#FF5733" },
-        insideThumbCover: { metalic: false, color: "red" },
-        trim: { metalic: false, color: "red" },
-        laces: { metalic: false, color: "red" },
-        backLogo : "./color-options/boxing-logos/boxing_logo.png"
+        Name: "Second", // Name 
+        img : "/color-options/popular-designs/red.png", // Thumbnail Image
+        fist: { metalic: false, color: "red" }, // Fist Color
+        frontThumb: { metalic: false, color: "red" }, // Front Thumb Color
+        wristColor: { metalic: false, color: "red" }, // Wrist Color
+        internalPalm: { metalic: false, color: "red" }, // internal Palm and Wrist Color
+        innerThumb: { metalic: false, color: "red" }, //Inside Part Of Thumb Color
+        insideThumbCover: { metalic: false, color: "red" }, // Inner Thumb Cover Color
+        trim: { metalic: false, color: "red" }, //Trim Color 
+        laces: { metalic: false, color: "red" }, // Laces Color
+        backLogo : NegroClasico
       },
+      {
+        Name: "Third", // Name 
+        img : "/color-options/popular-designs/gold.png", // Thumbnail Image
+        fist: { metalic: true, color: "#D4AF37" }, // Fist Color
+        frontThumb: { metalic: true, color: "#D4AF37" }, // Front Thumb Color
+        wristColor: { metalic: true, color: "#D4AF37" }, // Wrist Color
+        internalPalm: { metalic: true, color: "#D4AF37" }, // internal Palm and Wrist Color
+        innerThumb: { metalic: true, color: "#D4AF37" }, //Inside Part Of Thumb Color
+        insideThumbCover: { metalic: true, color: "#D4AF37" }, // Inner Thumb Cover Color
+        trim: { metalic: true, color: "#D4AF37" }, //Trim Color 
+        laces: { metalic: false, color: "gold" }, // Laces Color
+        backLogo : VivaMexico // Back Logo
+      },
+
+
+
+  
     ];
   
     // Use Zustand store
