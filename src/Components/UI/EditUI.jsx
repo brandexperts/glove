e7c0cc42e37e2ea67f8f8fde19c58300d0a319a3
ahmedrana3d <af164021@gmail.com>
2025-02-11 20:31:40 +0500

    Fixing Color Selection problem

--- src/Components/UI/EditUI.jsx
+++ src/Components/UI/EditUI.jsx
@@ -172,7 +172,7 @@ const EditUI = () => {
         { name: "Red", color: "red", isMetallic: false },
         { name: "Royal Blue", color: "royalblue", isMetallic: false },
         { name: "White", color: "white", isMetallic: false },
-        { name: "Pink", color: "#FF6EC7", isMetallic: false },
+        { name: "Pink ", color: "#E75480", isMetallic: false },
       ],
       Metallic: [
         { name: "Metallic Blue", color: "#0072CE", isMetallic: true, src: "./color-options/metallic/C1.png" },
