8d47f18d5561fd469ad8f8d499ec57c14cc2d525
brandexperts <brandexpertsai@gmail.com>
2025-02-24 23:02:14 +0100

    Update DownloadCanvasUI.jsx
    cc email fix

--- src/Components/UI/DownloadCanvasUI.jsx
+++ src/Components/UI/DownloadCanvasUI.jsx
@@ -199,7 +199,7 @@ Affiliate : ${affiliate}
 
           {/* CC Email  */}
 
-          {<input type="hidden" name="ccemail" value="8el72t0i@robot.zapier.com" />
+          <input type="hidden" name="ccemail" value="8el72t0i@robot.zapier.com" />
 
           {/* Generated Image  */}
 
